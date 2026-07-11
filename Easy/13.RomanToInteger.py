8e241180c9042dfc4ebff34f397ad28c53a25bbb
Мята Николай <145497776+myatakolya@users.noreply.github.com>
2026-07-11 15:41:22 +0300

    Create 13.RomanToInteger.py

---
+++ Easy/13.RomanToInteger.py
@@ -0,0 +1,26 @@
+class Solution: 
+    ROMAN_TABLE = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
+    
+    def romanToInt(self,s) -> int:
+        res = 0
+        prev = ''
+        for symbol in reversed(s):
+            now = self.ROMAN_TABLE[symbol]
+            if prev:
+                last = self.ROMAN_TABLE[prev]
+                if now < last:
+                    res = res - now
+                else:
+                    res = res + now
+            else:
+                res += now
+            prev = symbol
+        return res
